d520bc649347a04fea5df3f4a8fbfcc4b2615431
Paul Nadola Oyinga <nadolapaul@gmail.com>
2023-03-30 13:56:53 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,87 +1,58 @@
 /* Your Code Here */
-let employeeRecords = []
 function createEmployeeRecord(employee) {
-    return {
-      firstName: employee[0],
-      familyName: employee[1],
-      title: employee[2],
-      payPerHour: employee[3],
-      timeInEvents: [],
-      timeOutEvents: []
-    }
+  return {
+    firstName: employee[0],
+    familyName: employee[1],
+    title: employee[2],
+    payPerHour: employee[3],
+    timeInEvents: [],
+    timeOutEvents: []
   }
-  
-  function createEmployeeRecords(employees) {
-    return employees.map(employee => createEmployeeRecord(employee));
-  }
-  
-  function createTimeInEvent(dateStamp) {
-    const [date, hour] = dateStamp.split(" ");
-    this.timeInEvents.push({
-      type: "TimeIn",
-      hour: parseInt(hour),
-      date: date
-    });
-    return this;
-  }
-  
-  function createTimeOutEvent(dateStamp) {
-    const [date, hour] = dateStamp.split(" ");
-    this.timeOutEvents.push({
-      type: "TimeOut",
-      hour: parseInt(hour),
-      date: date
-    });
-    return this;
-  }
-  
-  function hoursWorkedOnDate(date) {
-    const timeIn = this.timeInEvents.find(event => event.date === date);
-    const timeOut = this.timeOutEvents.find(event => event.date === date);
-    return (timeOut.hour - timeIn.hour) / 100;
-  }
-  
-  function wagesEarnedOnDate(date) {
-    const hours = hoursWorkedOnDate.call(this, date);
-    return hours * this.payPerHour;
-  }
-  
-  function allWagesFor() {
-    const dates = this.timeInEvents.map(event => event.date);
-    const wages = dates.reduce((total, date) => {
-      return total + wagesEarnedOnDate.call(this, date);
-    }, 0);
-    return wages;
-  }
-  
-  function findEmployeeByFirstName(firstName) {
-    return employeeRecords.find(record => record.firstName === firstName);
-  }
-  
-  function calculatePayroll(employeeRecords) {
-    return employeeRecords.reduce((total, record) => {
-      return total + allWagesFor.call(record);
-    }, 0);
-  }
-  
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+}
+function createEmployeeRecords(employees) {
+  return employees.map(employee => createEmployeeRecord(employee));
+}
+function createTimeInEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  this.timeInEvents.push({
+    type: "TimeIn",
+    hour: parseInt(hour),
+    date: date
+  });
+  return this;
+}
+function createTimeOutEvent(dateStamp) {
+  const [date, hour] = dateStamp.split(" ");
+  this.timeOutEvents.push({
+    type: "TimeOut",
+    hour: parseInt(hour),
+    date: date
+  });
+  return this;
+}
+function hoursWorkedOnDate(date) {
+  const timeIn = this.timeInEvents.find(event => event.date === date);
+  const timeOut = this.timeOutEvents.find(event => event.date === date);
+  return (timeOut.hour - timeIn.hour) / 100;
+}
+function wagesEarnedOnDate(date) {
+  const hours = hoursWorkedOnDate.call(this, date);
+  return hours * this.payPerHour;
+}
+function allWagesFor() {
+  const dates = this.timeInEvents.map(event => event.date);
+  const wages = dates.reduce((total, date) => {
+    return total + wagesEarnedOnDate.call(this, date);
+  }, 0);
+  return wages;
+}
+function findEmployeeByFirstName(employeeRecords, str) {
+  let found = employeeRecords.find(record => record.firstName === str);
+  return found
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
-
-//  const allWagesFor = function () {
-//     const eligibleDates = this.timeInEvents.map(function (e) {
-//         return e.date
-//     })
-
-//     const payable = eligibleDates.reduce(function (memo, d) {
-//         return memo + wagesEarnedOnDate.call(this, d)
-//     }.bind(this), 0) // <== Hm, why did we need to add bind() there? We'll discuss soon!
-
-//     return payable
-// }
-
+function calculatePayroll(employeeRecords) {
+  return employeeRecords.reduce((total, record) => {
+    return total + allWagesFor.call(record);
+  }, 0);
+}
